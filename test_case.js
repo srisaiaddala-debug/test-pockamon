c0971dfa475e47dfa044067f3cd9915038182822
praveengargit <praveengargit@gmail.com>
2026-04-14 12:24:37 +0530

    Update test_case.js

--- test_case.js
+++ test_case.js
@@ -8,3 +8,14 @@ getMostViewedFaqs.mockResolvedValue([
   { faqId: '1', viewCount: 10 },
   { faqId: '2', viewCount: 5 },
 ]);
+
+
+// ✅ ADD THIS
+  if (url.includes('/faq/most-viewed')) {
+    return Promise.resolve({
+      data: [
+        { faqId: '1', viewCount: 10 },
+        { faqId: '2', viewCount: 5 },
+      ],
+    });
+  }
